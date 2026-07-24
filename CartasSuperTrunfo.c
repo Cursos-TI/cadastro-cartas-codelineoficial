5dc3f33d547b874bec4625ef3950a67d0accbaa4
codelineoficial <75189442+codelineoficial@users.noreply.github.com>
2026-07-23 21:38:42 -0300

    Update CartasSuperTrunfo.c
    Finalização da Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,112 @@
 #include <stdio.h>
+#include <windows.h> 
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    SetConsoleOutputCP(CP_UTF8);
 
-  // Área para entrada de dados
+    // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado1;
+    char codigo1[10];         // Espaço para letra + 2 números + 8 folga caso o usuario digite errado + '\0'
+    char nome_cidade1[50];    // Limite de 49 caracteres + '\0'
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos_turisticos1;
 
-  // Área para exibição dos dados da cidade
+    char estado2;
+    char codigo2[10];          // Espaço para letra + 2 números + 8 folga caso o usuario digite errado + '\0'
+    char nome_cidade2[50];     // Limite de 49 caracteres + '\0'
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
+    
+    // Área para entrada de dados
+
+    // Intro
+    printf("Bem-vindo ao Desafio Super Trunfo! ^^\n");
+    printf("\n");
+    printf("Nesse jogo, iremos competir para determinar quem tem a carta mais forte.\n");
+    printf("Vamos começar cadastrando sua primeira carta ;)\n");
+
+    // Cadastro da carta 1
+    printf("Digite o Estado (uma letra de A a H): ");
+    scanf(" %c", &estado1); // O espaço antes de %c limpa o buffer do teclado
+
+    printf("Digite o Código da Carta (ex: A01): ");
+    scanf("%s", codigo1); // Lê o código inteiro de uma vez (sem deixar sobra no buffer)
+
+    printf("Digite o Nome da Cidade: ");
+    scanf(" %[^\n]", nome_cidade1); // Lê o nome completo incluindo espaços
+
+    printf("Digite a População: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Digite o Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+    printf("\n");
+
+    printf("Agora, vamos cadastrar sua segunda carta:\n");
+
+    // Cadastro da carta 2
+    printf("Digite o Estado (uma letra de A a H): ");
+    scanf(" %c", &estado2);
+
+    printf("Digite o Código da Carta (ex: B02): ");
+    scanf("%s", codigo2); // Lê o código inteiro de uma vez (sem deixar sobra no buffer)
+
+    printf("Digite o Nome da Cidade: ");
+    scanf(" %[^\n]", nome_cidade2); 
+
+    printf("Digite a População: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Área (em km²): ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Digite o Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("\n========================================\n\n");
+
+    printf("Agora sim, você está pronto para a batalha!\n");
+    printf("Suas cartas disponíveis são:\n");
+    
+
+    // Área para exibição dos dados da cidade
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nome_cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+
+    printf("\n");
+
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome_cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
 
 return 0;
 } 
